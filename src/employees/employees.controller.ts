f08f3cc5d0f12b386f6a3b90224681ab6ac8f02a
azacdev <azacdev@gmail.com>
2024-09-06 23:24:28 +0100

    cors, rate limiter, logs

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -8,10 +8,13 @@ import {
   Delete,
   Query,
 } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { Throttle, SkipThrottle } from '@nestjs/throttler';
 
 import { EmployeesService } from './employees.service';
-import { Prisma } from '@prisma/client';
 
+// Skip rate limiter for all routes
+@SkipThrottle()
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
@@ -21,11 +24,15 @@ export class EmployeesController {
     return this.employeesService.create(createEmployessDto);
   }
 
+  // Dont skip rate limiter for this route
+  @SkipThrottle({ default: false })
   @Get()
   findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
     return this.employeesService.findAll(role);
   }
 
+  // Override rate limiter for this route
+  @Throttle({ short: { ttl: 1000, limit: 1 } })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.employeesService.findOne(id);
